f9eeeb75e061360207e8062623456666fd618086
Noelia Morato Caero <cbbe.noelia.morato.ca@unifranz.edu.bo>
2024-09-15 09:43:35 -0400

    Login

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Login from "./pages/public/login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
